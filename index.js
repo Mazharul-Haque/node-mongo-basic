6620e80213518eb561274aa8116bb28ef8d64684
Mazharul-Haque <mshagor009@gmail.com>
2021-04-09 15:10:18 +0600

    data load from api

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+const users= ['apple', 'banana', 'orange']
+
 app.get('/', (req, res)=>{
-    res.send('Thank you for calling, i know how to open node');
+    const team = {
+        name : 'Manchester City',
+        League : 'UCL'
+
+    }
+    res.send(team);
+});
+app.get('/fruits/banana', (req, res)=> {
+    res.send({fruit:'banana', quantity : 1000, price:10000});
+});
+//users
+app.get('/users/:id', (req, res)=>{
+    const id= req.params.id;
+    console.log(req.query);
+    const name = users[id];
+    res.send({id, name});
+
 })
 
 app.listen(3000, ()=> console.log("Connecting...to 3000"));
